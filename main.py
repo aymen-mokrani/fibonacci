e95bfae9e9601f4b0a6ba8504b50982a17d08731
aymen <aymen.mokrani@g.enp.edu.dz>
2023-09-16 20:10:29 +0200

    Tp 1 complixité ( fibonacci )

--- main.py
+++ main.py
@@ -19,43 +19,53 @@ def fib_iter(x):
             b = c
         return c
 
-
-def puissance(n):
-    M = np.array([[1, 1], [1, 0]])
-    i = 0
-    result= 1
-    if n==1 or n==0:
-        return 1
-
-    elif np.log2(n) is int:
-        k=np.log2(n)
-        while (i < k-1):
+def calcul_de_puissance_expo(M,n):
+    i=1
+    while (i < n+1):
             i += 1
             M = np.dot(M,M)
+    return M
+
+def calcul_de_puissance(M,n):
+    i=1
+    result = M
+    while (i < n):
+        i += 1
+        result = np.dot(M,M)
+    return result
+
+
+def puissance(M,n):
+    
+    if n==1 or n==0:
         return M
+
+    elif np.log2(n).is_integer():
+        k=np.log2(n)
+        return calcul_de_puissance_expo(M,k)
 
     elif n % 2 == 0:
         k=n/2
-        while (i < k+1):
-            i += 1
-            result = np.dot(M,result)
+        result = puissance(M,k)
         return np.dot(result,result)
+    
     else:
         k=n-1
-        while (i < k+1):
-            i+=1
-            result = np.dot(M,result)
+        result = puissance(M,k)
         return np.dot(M,result)
+    
+
 def fib_mat(x):
     M = np.array([[1, 1], [1, 0]])
-    fib= np.array([0, 1])
-    result = np.dot(fib,puissance(x))
+    fib= np.array([1, 0])
+    result = np.dot(fib,puissance(M,x))
 
 
-    return result[1]
+    return result[0]
 
 
-n=input('Please put your number:')
-print(fib_iter(int(n)))
-print(fib_rec(int(n)))
-print(fib_mat(int(n)))
+n=input('Entrez votre numero :')
+print()
+print(f'Avec la methode d\'iteration : {fib_iter(int(n))}')
+print(f'Avec la methode reciproque : {fib_rec(int(n))}')
+print(f'Avec la methode matricielle : {fib_mat(int(n))}')
